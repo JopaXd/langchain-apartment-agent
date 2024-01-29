f1823054ae3e8157e75daa232da67c1b2fe248a7
Pavle Milanov <pavle_milanov@hotmail.com>
2024-01-29 03:07:37 +0100

    Created booking tool.

---
+++ booking_tool.py
@@ -0,0 +1,112 @@
+from langchain_community.document_transformers import BeautifulSoupTransformer
+from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import AsyncHtmlLoader
+from langchain_community.tools import DuckDuckGoSearchResults
+from langchain.tools import BaseTool, StructuredTool, tool
+from langchain.pydantic_v1 import BaseModel, Field
+from langchain.callbacks.manager import (
+	AsyncCallbackManagerForToolRun,
+	CallbackManagerForToolRun,
+)
+from kor import create_extraction_chain, Object, Text, Bool
+from typing import Optional, Type, Union
+from g4fllm import G4FLLM
+import pprint
+import re
+
+class BookingInput(BaseModel):
+	location: str = Field(description="Location where the user wants to travel/location of the apartment.")
+	checkInDate: str = Field(description="Date of checking in. Example: 2024-08-05")
+	checkOutDate: str = Field(description="Date of checking out. Example: 2024-08-05")
+	numberOfAdults: int =  Field(description="Number of adults that will be in the apartment.")
+	numberOfChildren: int =  Field(description="Number of children that will be in the apartment.")
+	numberOfRooms: int = Field(description="Number of rooms in the apartment")
+	#Due to the possibility of "min" and "max" values, these should be string.
+	minimumPricePerNight: str = Field(description="The minimum that the user is willing to spend per night (in euros). The lower value of the price range.")
+	maximumPricePerNight: str = Field(description="The maximum that the user is willing to spend per night (in euros). The higher value of the price range.")
+
+class BookingTool(BaseTool):
+	name = "Booking"
+	description = (
+		"Use this tool when you need to find, and recommend the best apartments according to the user input."
+		"Given the check in and check out date, number of rooms in the apartment, number of adults and children in the apartment, and minimum and maximum price per night"
+		"To use the tool you must provide all of the following parameters "
+		"['location', 'checkInDate', 'checkOutDate', 'numberOfAdults', 'numberOfRooms', 'numberOfChildren', 'minimumPricePerNight', 'maximumPricePerNight']."
+	)
+	args_schema: Type[BaseModel] = BookingInput
+	return_direct: bool = True
+
+	def _extract(self, content:str, schema):
+		llm = G4FLLM()
+		return create_extraction_chain(llm, schema, encoder_or_encoder_class='json').invoke(content)
+
+	# Sometimes, the response is not returned fully according to the schema, thus producing errors.
+	# Apparently, better description can help mitigate this.
+	def _scrape_booking(self, url):
+		schema = Object(
+			id="apartments",
+			description = ("Apartments that the user might be interested in according to their input."),
+			attributes=[
+				Text(id="apartment_name", description="Name of apartment"),
+				# Text(id="apartment_link", description="Link of the page where the user can look at the apartment", examples=[("https://booking.com/hotel/bg...", "Example of the link of an appartment")]),
+				Text(id="apartment_location", description="Location of the apartment"),
+				Text(id="apartment_distance_from_center", description="Distance of the apartment from the city center"),
+				Text(id="apartment_distance_from_beach", description="Distance of the apartment from the beach"),
+				Bool(id="apartment_free_cancellation", description="Can the apartment reservation be cancelled for free"),
+				Text(id="apartment_taxes", description="Taxes for apartment"),
+				Text(id="apartment_description", description="Description of apartment, for example, rooms, etc..."),
+				Text(id="apartment_price", description="Price of the apartment, the lower price that is present."),
+				Text(id="apartment_rating", description="Rating of the apartment")
+			],
+			many=True
+		)
+		loader = AsyncHtmlLoader([url])
+		docs = loader.load()
+		for doc in docs:
+			doc.page_content = doc.page_content.replace(u"\xa0", u"").strip()
+		bs_transformer = BeautifulSoupTransformer()
+		docs_transformed = bs_transformer.transform_documents(docs, tags_to_extract=["div", 'span', 'p', 'li', 'a', 'strong'])
+		print("Extracting content with LLM")
+		splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=2048, chunk_overlap=0)
+		splits = splitter.split_documents(docs_transformed)
+		extracted_content = self._extract(splits[0].page_content, schema)
+		pprint.pprint(extracted_content)
+		# print(extracted_content["text"]["data"]["apartments"])
+		wrapper = DuckDuckGoSearchAPIWrapper(region="wt-wt", time=None, max_results=3)
+		search = DuckDuckGoSearchResults(api_wrapper=wrapper, source="text")
+		for apartment in extracted_content["text"]["data"]["apartments"]:
+			apartment_url = re.search("(?P<url>https?://[^\s]+)", search.run(f"{apartment['apartment_name']} {apartment['apartment_location']} booking")).group("url")[:-2]
+			if apartment_url:
+				apartment["apartment_url"] = apartment_url
+		return extracted_content["text"]["data"]["apartments"]
+
+
+	def _run(
+		self,
+		location:str,
+		checkInDate:str,
+		checkOutDate:str,
+		numberOfAdults:int,
+		numberOfChildren:int,
+		numberOfRooms:int,
+		minimumPricePerNight:str="min",
+		maximumPricePerNight:str="max",
+		run_manager: Optional[CallbackManagerForToolRun] = None
+		) -> str:
+		"""Use the tool."""
+		return str(self._scrape_booking(f"https://www.booking.com/searchresults.html?ss={location}&lang=en-us&checkin={checkInDate}&checkout={checkOutDate}&group_adults={numberOfAdults}&no_rooms={numberOfRooms}&group_children={numberOfChildren}&nflt=price%3DEUR-{minimumPricePerNight}-{maximumPricePerNight}-1&selected_currency=EUR"))
+
+	def _arun(
+		self,
+		location:str,
+	 	checkInDate:str,
+	 	checkOutDate:str,
+	 	numberOfAdults:int,
+	  	numberOfChildren:int,
+	  	numberOfRooms:int,
+	  	minimumPricePerNight:str="min",
+	  	maximumPricePerNight:str="max",
+	  	run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
+		"""Use the tool asynchronously."""
+		raise NotImplementedError("Calculator does not support async")
--- main.py
+++ main.py
@@ -1,61 +1,14 @@
-from langchain_community.document_transformers import BeautifulSoupTransformer
-from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import AsyncHtmlLoader
-from langchain_community.tools import DuckDuckGoSearchResults
-from kor import create_extraction_chain, Object, Text, Bool
+from langchain.agents import AgentExecutor, create_structured_chat_agent
+from booking_tool import BookingTool
+from langchain import hub
 from g4fllm import G4FLLM
-import pprint
-import re
-
-schema = Object(
-		id="apartments",
-		description = ("Apartments that the user might be interested in according to their input."),
-		attributes=[
-			Text(id="apartment_name", description="Name of apartment"),
-			# Text(id="apartment_link", description="Link of the page where the user can look at the apartment", examples=[("https://booking.com/hotel/bg...", "Example of the link of an appartment")]),
-			Text(id="apartment_location", description="Location of the apartment"),
-			Text(id="apartment_distance_from_center", description="Distance of the apartment from the city center"),
-			Text(id="apartment_distance_from_beach", description="Distance of the apartment from the beach"),
-			Bool(id="apartment_free_cancellation", description="Can the apartment reservation be cancelled for free"),
-			Text(id="apartment_taxes", description="Taxes for apartment"),
-			Text(id="apartment_description", description="Description of apartment, for example, rooms, etc..."),
-			Text(id="apartment_price", description="Price of the apartment, the lower price that is present."),
-			Text(id="apartment_rating", description="Rating of the apartment")
-		],
-		many=True
-	)
 
 llm = G4FLLM()
 
-def extract(content:str, schema):
-	print(content)
-	return create_extraction_chain(llm, schema, encoder_or_encoder_class='json').invoke(content)
-
-# Sometimes, the response is not returned fully according to the schema, thus producing errors.
-# Apparently, better description can help mitigate this.
-def scrape_booking(urls, schema):
-	loader = AsyncHtmlLoader(urls)
-	docs = loader.load()
-	for doc in docs:
-		doc.page_content = doc.page_content.replace(u"\xa0", u"").strip()
-	bs_transformer = BeautifulSoupTransformer()
-	docs_transformed = bs_transformer.transform_documents(docs, tags_to_extract=["div", 'span', 'p', 'li', 'a', 'strong'])
-	print("Extracting content with LLM")
-	splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=2048, chunk_overlap=0)
-	splits = splitter.split_documents(docs_transformed)
-	extracted_content = extract(splits[0].page_content, schema)
-	# pprint.pprint(extracted_content)
-	# print(extracted_content["text"]["data"]["apartments"])
-	wrapper = DuckDuckGoSearchAPIWrapper(region="wt-wt", time=None, max_results=3)
-	search = DuckDuckGoSearchResults(api_wrapper=wrapper, source="text")
-	for apartment in extracted_content["text"]["data"]["apartments"]:
-		apartment_url = re.search("(?P<url>https?://[^\s]+)", search.run(f"Hotel Manz 2 {apartment['apartment_location']} booking")).group("url")[:-2]
-		if apartment_url:
-			apartment["apartment_url"] = apartment_url
-	return extracted_content["text"]["data"]["apartments"]
-
-urls = ["https://www.booking.com/searchresults.html?ss=Pomorie%2C+Burgas+Province%2C+Bulgaria&lang=en-us&checkin=2024-07-27&checkout=2024-08-05&group_adults=2&no_rooms=1&group_children=0&nflt=price%3DEUR-min-40-1&selected_currency=EUR"]
-
-extracted_content = scrape_booking(urls, schema=schema)
-pprint.pprint(extracted_content)
+if __name__ == "__main__":
+	tools = [BookingTool()]
+	prompt = hub.pull("hwchase17/structured-chat-agent")
+	llm = G4FLLM()
+	agent = create_structured_chat_agent(llm, tools, prompt)
+	agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True, max_iterations=10)
+	agent_executor.invoke({"input": "I am looking for an apartment in Pomorie for two with one room from 26th of July, to 5th of august this year. The price for one night should be between 10 and 40 euros."})
