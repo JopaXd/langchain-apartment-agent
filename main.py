694d4f42b2afd155cc5ba3c12856a801c7e69079
Pavle Milanov <pavle_milanov@hotmail.com>
2024-02-01 15:17:43 +0100

    More fixes to booking tool, added chainlit ui.

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ from langchain.agents import AgentExecutor, create_structured_chat_agent
 from booking_tool import BookingTool
 from langchain import hub
 from g4fllm import G4FLLM
+import chainlit as cl
 
 llm = G4FLLM()
 
-if __name__ == "__main__":
+@cl.on_chat_start
+def start():
 	tools = [BookingTool()]
 	prompt = hub.pull("hwchase17/structured-chat-agent")
 	llm = G4FLLM(gpt_model="gpt-4")
 	agent = create_structured_chat_agent(llm, tools, prompt)
 	agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True, max_iterations=10)
-	agent_executor.invoke({"input": "I am looking for an apartment in Pomorie for two with one room from 26th of July, to 5th of august this year. The price for one night should be between 10 and 50 CAD."})
+	cl.user_session.set("agent", agent_executor)
+
+@cl.on_message
+async def main(message):
+	agent = cl.user_session.get("agent")
+	cb = cl.LangchainCallbackHandler(stream_final_answer=True)
+	agent.callbacks = [cb]
+	await cl.make_async(agent.invoke)({"input": message.content}, callbacks=[cb])
