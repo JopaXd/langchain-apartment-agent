8c1761efa8b010dfee1e407b239feeae364e1c07
Pavle Milanov <pavle_milanov@hotmail.com>
2024-02-06 16:08:54 +0100

    The agent now has short-term memory of the chat. (Meaning the memory only lasts during the session).

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from langchain.agents import AgentExecutor, create_structured_chat_agent
+from langchain.memory import ConversationBufferMemory
 from booking_tool import BookingTool
 from langchain import hub
 from g4fllm import G4FLLM
@@ -9,16 +10,17 @@ llm = G4FLLM()
 @cl.on_chat_start
 def start():
 	tools = [BookingTool()]
+	memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 	prompt = hub.pull("hwchase17/structured-chat-agent")
 	#Extracted from hwchase17/structured-chat-agent, 
 	prompt.messages[0].prompt.template = """
 		'Respond to the human as helpfully and accurately as possible.
-		 When using the booking tool (if available), return the list of the best apartments according to the users input in response. The response should contain all the data about the apartments you have, including the link where the user can look at an apartment.
+		 When using the booking tool (if available), return the list of the best apartments according to the users input in response. The response should contain all the data about the apartments you have chosen to be the best, including the link where the user can look at an apartment.
 		 You have access to the following tools:\n\n{tools}\n\nUse a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).\n\nValid "action" values: "Final Answer" or {tool_names}\n\nProvide only ONE action per $JSON_BLOB, as shown:\n\n```\n{{\n  "action": $TOOL_NAME,\n  "action_input": $INPUT\n}}\n```\n\nFollow this format:\n\nQuestion: input question to answer\nThought: consider previous and subsequent steps\nAction:\n```\n$JSON_BLOB\n```\nObservation: action result\n... (repeat Thought/Action/Observation N times)\nThought: I know what to respond\nAction:\n```\n{{\n  "action": "Final Answer",\n  "action_input": "Final response to human"\n}}\n\nBegin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation'
 	"""
-	llm = G4FLLM(gpt_model="gpt-4")
+	llm = G4FLLM(gpt_model="gpt-3.5")
 	agent = create_structured_chat_agent(llm, tools, prompt)
-	agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True, max_iterations=10)
+	agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True, handle_parsing_errors=True, max_iterations=10)
 	cl.user_session.set("agent", agent_executor)
 
 @cl.on_message
